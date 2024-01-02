1bd8c435acabf360d8ef0a78428290d024ae7836
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-01-02 12:57:29 -0600

    bcrypt and jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
+
   def current_user
     User.first
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+
+
   def create
     user = User.new(
       name: params[:name],
